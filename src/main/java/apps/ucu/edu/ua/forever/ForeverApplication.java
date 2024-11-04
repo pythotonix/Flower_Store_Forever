bda316fb02440009930e3a4a379f6fb1fc408380
Tetiana Shvets <shvets.pn@ucu.edu.ua>
2024-11-04 19:45:03 +0200

    fix style final

--- src/main/java/apps/ucu/edu/ua/forever/ForeverApplication.java
+++ src/main/java/apps/ucu/edu/ua/forever/ForeverApplication.java
@@ -1,7 +1,7 @@
 package apps.ucu.edu.ua.forever;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class ForeverApplication {
